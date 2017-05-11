ce19e4dacb0076300cafdfb795508fd933d8951a
Matteo Joliveau <matteojoliveau@gmail.com>
2017-05-11 23:32:50 +0200

    Git management section in Readme

---
+++ src/frontend/index.jsx
@@ -0,0 +1,38 @@
+require('file-loader?name=[name].[ext]!./index.html');
+
+/* React Framework Components */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+//import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+
+/* Custom React Components */
+import HomePage from './components/pages/HomePage.jsx';
+import FormPage from './components/pages/FormPage.jsx';
+import ProjectListPage from './components/pages/ProjectListPage.jsx';
+
+const AppRouter = () => (
+
+  <Router>
+    <div>
+      <nav className="navbar navbar-default">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">Open Source Forum</Link>
+          </div>
+          <ul className="nav navbar-nav">
+            <li><Link to="/submit">Submit a Project</Link></li>
+            <li><Link to="/projects">All the Projects</Link></li>
+          </ul>
+          </div>
+      </nav>
+
+
+      <Route exact path="/" component={HomePage}/>
+      <Route path = "/submit" component={FormPage}/>
+      <Route path = "/projects" component={ProjectListPage}/>
+    </div>
+  </Router>
+)
+ReactDOM.render(
+  <AppRouter />, document.getElementById("react-app"));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'build');
+const APP_DIR = path.resolve(__dirname, 'src/frontend');
+
+const config = {
+  context: APP_DIR,
+  entry: {
+    app: './index.jsx'
+  },
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+    publicPath: "/assets/"
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;

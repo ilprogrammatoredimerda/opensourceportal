4d490c90f45ff3de8c20c2747d09619d9fa41446
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-05-21 14:01:52 +0200

    config

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,46 +1,46 @@
-const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-
-const app = express();
+const app = require('./express-app');
 
 /* Send the index.html containing React bootstrap code */
 const sendIndexFile = (res) => {
-  res.sendFile(path.resolve(__dirname + "/../../build/index.html"));
+	res.sendFile(path.resolve(__dirname + "/../../build/index.html"));
 }
 
 /* Resolve all static files to this path */
 app.use(express.static(path.resolve(__dirname + "/../../build")));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
 app.use(bodyParser.json());
 
 /*
-* Map all the URI paths to avoid React Router to
-* lose the page in case of redirect or manual URL search
-*/
+ * Map all the URI paths to avoid React Router to
+ * lose the page in case of redirect or manual URL search
+ */
 app.get("/", (req, res) => {
-  sendIndexFile(res);
+	sendIndexFile(res);
 })
 
 app.get("/submit", (req, res) => {
-  sendIndexFile(res);
+	sendIndexFile(res);
 })
 
 app.get("/projects", (req, res) => {
-  sendIndexFile(res);
+	sendIndexFile(res);
 })
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/../../build/404.html"))
+	res.sendFile(path.resolve(__dirname + "/../../build/404.html"))
 })
 
 //dummy for tests
 app.post("/api/newProject", (req, res) => {
-    console.dir(req.body)
-    res.sendStatus(200);
+	console.dir(req.body)
+	res.sendStatus(200);
 });
 
 /* Start the server */
 app.listen(3000, () => {
-  console.log("OSF listening on port 3000");
+	console.log("OSF listening on port 3000");
 })

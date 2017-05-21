e8ca83cb6d416e281609aea8dbf9370a08c97891
Arcangelo Vicedomini <io@arcangelovicedomini.eu>
2017-05-21 11:28:58 +0200

    database entity from italian to english

--- src/backend/database.js
+++ src/backend/database.js
@@ -1,187 +1,186 @@
+'use strict';
 
-	'use strict';
+const Utils = require('./utils.js');
+const Sequelize = require('sequelize');
+const Config = require('./config.js');
 
-	const Utils = require('./utils.js');
-	const Sequelize = require('sequelize');
-	const Config = require('./config.js');
+var connection = new Sequelize(Config.get("DB_URL"), {
+	dialect: Config.get('DB_DIALECT'),
 
-	var connection = new Sequelize(Config.get("DB_URL"), {
-		dialect: Config.get('DB_DIALECT'),
+	pool: {
+		max: Config.get('DB_POOL_MAX'),
+		min: Config.get('DB_POOL_MIN'),
+		idle: Config.get('DB_POOL_IDLE')
+	},
 
-		pool: {
-			max: Config.get('DB_POOL_MAX'),
-			min: Config.get('DB_POOL_MIN'),
-			idle: Config.get('DB_POOL_IDLE')
-		},
+	define: {
+		// don't use camelcase for automatically added attributes but underscore style
+		// so updatedAt will be updated_at
+		underscored: true,
 
-		define: {
-			// don't use camelcase for automatically added attributes but underscore style
-			// so updatedAt will be updated_at
-			underscored: true,
+		// disable the modification of tablenames; By default, sequelize will automatically
+		// transform all passed model names (first parameter of define) into plural.
+		// if you don't want that, set the following
+		freezeTableName: true
+	}
+});
+connection.authenticate().complete(function (err) {
+	if (err) {
+		console.error('Unable to connect to the database:', err);
+	} else {
+		console.debug('Connection has been established successfully.');
+	}
+});
 
-			// disable the modification of tablenames; By default, sequelize will automatically
-			// transform all passed model names (first parameter of define) into plural.
-			// if you don't want that, set the following
-			freezeTableName: true
+var mapping = {};
+mapping.Project = connection.define('project', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	codice: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	},
+	titolo: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	descrizioneBreve: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		field: "descrizione_breve"
+	},
+	descrizioneCompleta: {
+		type: Sequelize.TEXT,
+		allowNull: false,
+		field: "descrizione_completa"
+	},
+	immagine: {
+		type: Sequelize.BLOB('long'),
+		allowNull: false
+	}
+});
+
+mapping.ProjectCategory = connection.define('project_category', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	}
+});
+
+mapping.Category = connection.define('category', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	codice: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	},
+	titolo: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	descrizione: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	immagine: {
+		type: Sequelize.BLOB('long'),
+		allowNull: false
+	}
+});
+
+mapping.Project.hasMany(mapping.ProjectCategory, {
+	as: 'categories',
+	foreignKey: 'id_project'
+});
+
+mapping.ProjectCategory.belongsTo(mapping.Project, {
+	as: 'project',
+	foreignKey: 'id_project'
+});
+
+mapping.Category.hasMany(mapping.ProjectCategory, {
+	as: 'projects',
+	foreignKey: 'id_category'
+});
+
+mapping.ProjectCategory.belongsTo(mapping.Category, {
+	as: 'category',
+	foreignKey: 'id_category'
+});
+
+mapping.RoleProject = connection.define('role_project', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	codice: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	}
+});
+
+mapping.User = connection.define('user', {
+	id: {
+		type: Sequelize.INTEGER,
+		primaryKey: true,
+		autoIncrement: true
+	},
+	username: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false
+	},
+	email: {
+		type: Sequelize.STRING,
+		unique: true,
+		allowNull: false
+	},
+	password: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+}, {
+	setterMethods: {
+		password: function (value) {
+			this.setDataValue('password', Utils.passwordChipher(value));
 		}
-	});
-	connection.authenticate().complete(function(err) {
-		if (err) {
-			console.error('Unable to connect to the database:', err);
-		} else {
-			console.debug('Connection has been established successfully.');
-		}
-	});
+	}
+});
 
-	var mapping = {};
-	mapping.Progetto = connection.define('progetto', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		codice: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			unique: true
-		},
-		titolo: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		descrizioneBreve: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			field: "descrizione_breve"
-		},
-		descrizioneCompleta: {
-			type: Sequelize.TEXT,
-			allowNull: false,
-			field: "descrizione_completa"
-		},
-		immagine: {
-			type: Sequelize.BLOB('long'),
-			allowNull: false
-		}
-	});
+mapping.Project.hasMany(mapping.RoleProject, {
+	as: 'users',
+	foreignKey: 'id_project'
+});
 
-	mapping.ProgettoCategoria = connection.define('progetto_categoria', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		}
-	});
+mapping.RoleProject.belongsTo(mapping.Project, {
+	as: 'project',
+	foreignKey: 'id_project'
+});
 
-	mapping.Categoria = connection.define('categoria', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		codice: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			unique: true
-		},
-		titolo: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		descrizione: {
-			type: Sequelize.STRING,
-			allowNull: false
-		},
-		immagine: {
-			type: Sequelize.BLOB('long'),
-			allowNull: false
-		}
-	});
+mapping.User.hasMany(mapping.RoleProject, {
+	as: 'projects',
+	foreignKey: 'id_user'
+});
 
-	mapping.Progetto.hasMany(mapping.ProgettoCategoria, {
-		as: 'categorie',
-		foreignKey: 'id_progetto'
-	});
+mapping.RoleProject.belongsTo(mapping.User, {
+	as: 'user',
+	foreignKey: 'id_user'
+});
 
-	mapping.ProgettoCategoria.belongsTo(mapping.Progetto, {
-		as: 'progetto',
-		foreignKey: 'id_progetto'
-	});
+connection.sync();
 
-	mapping.Categoria.hasMany(mapping.ProgettoCategoria, {
-		as: 'prodotti',
-		foreignKey: 'id_categoria'
-	});
-
-	mapping.ProgettoCategoria.belongsTo(mapping.Categoria, {
-		as: 'categoria',
-		foreignKey: 'id_categoria'
-	});
-
-	mapping.RuoloProgetto = connection.define('ruolo_progetto', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		codice: {
-			type: Sequelize.STRING,
-			allowNull: false,
-			unique: true
-		}
-	});
-
-	mapping.Utente = connection.define('utente', {
-		id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		username: {
-			type: Sequelize.STRING,
-			unique: true,
-			allowNull: false
-		},
-		email: {
-			type: Sequelize.STRING,
-			unique: true,
-			allowNull: false
-		},
-		password: {
-			type: Sequelize.STRING,
-			allowNull: false
-		}
-	}, {
-		setterMethods: {
-			password: function (value) {
-				this.setDataValue('password', Utils.passwordChipher(value));
-			}
-		}
-	});
-
-	mapping.Progetto.hasMany(mapping.RuoloProgetto, {
-		as: 'persone',
-		foreignKey: 'id_progetto'
-	});
-
-	mapping.RuoloProgetto.belongsTo(mapping.Progetto, {
-		as: 'progetto',
-		foreignKey: 'id_progetto'
-	});
-
-	mapping.Utente.hasMany(mapping.RuoloProgetto, {
-		as: 'progetti',
-		foreignKey: 'id_utente'
-	});
-
-	mapping.RuoloProgetto.belongsTo(mapping.Utente, {
-		as: 'utente',
-		foreignKey: 'id_utente'
-	});
-
-	connection.sync();
-
-	exports.database = {
-		"mapping": mapping,
-		"connection": connection
-	};
+exports.database = {
+	"mapping": mapping,
+	"connection": connection
+};
